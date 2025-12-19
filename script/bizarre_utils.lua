979e5a9ca16afdc47eaefe6eceb4502049689cfd
SirFoggyster <kaleemgthomas@gmail.com>
2025-12-19 16:37:20 -0400

    update

--- script/bizarre_utils.lua
+++ script/bizarre_utils.lua
@@ -1,9 +1,23 @@
 function BizarreCount(tp)
-    return Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,SET_BIZARRE)
+    return Duel.GetMatchingGroupCount(
+        Card.IsSetCard,
+        tp,
+        LOCATION_GRAVE,
+        0,
+        nil,
+        SET_BIZARRE
+    )
 end
 
 function HasAvdol(tp)
-    return Duel.IsExistingMatchingCard(function(c)
-        return c:IsSetCard(SET_BIZARRE) and c:IsCode(100000001)
-    end,tp,LOCATION_MZONE,0,1,nil)
+    return Duel.IsExistingMatchingCard(
+        function(c)
+            return c:IsSetCard(SET_BIZARRE) and c:IsCode(1000001)
+        end,
+        tp,
+        LOCATION_MZONE,
+        0,
+        1,
+        nil
+    )
 end
--- script/constants.lua
+++ script/constants.lua
@@ -1,6 +1,5 @@
 -- Bizarre / Phantom Flame constants
 SET_BIZARRE = 0xBA5
-
+dofile("script/bizarre_utils.lua")
 SET_PHANTOM = 0xBA6
-
 SET_REPLICA = 0xBA7
